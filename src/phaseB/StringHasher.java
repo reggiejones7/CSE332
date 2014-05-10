0547e87f3c1c4133e73ea4272358ae147b8afcdc
Reggie Jones <reggiejones7@hotmail.com>
2014-05-09 18:18:43 -0700

    Update StringHasher doc

--- src/phaseB/StringHasher.java
+++ src/phaseB/StringHasher.java
@@ -7,14 +7,18 @@ import providedCode.Hasher;
  * CSE 332
  * TA: Hye In Kim
  * 
- * --class description--
+ * StringHasher class is used for hashing Strings, while implementing Hasher<String>
  *
  */
 public class StringHasher implements Hasher<String> {
 	
+	/**
+	 * hash returns a positive integer of the hash of a given String
+	 * @param s is a String to be hashed
+	 * @return an int that represents the hash of the String
+	 */
 	@Override
 	public int hash(String s) {
-		//can use length and charat
 		int hash = 0;
 		for (int i = 0; i < s.length(); i++) {
 			hash += (int) (s.charAt(i) * Math.pow(37, i));

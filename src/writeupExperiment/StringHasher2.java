3c7ddee4773cd88cf31d3708a2d83cf91cdf77d5
Reggie Jones <reggiejones7@hotmail.com>
2014-05-11 16:04:59 -0700

    Write up experiments

---
+++ src/writeupExperiment/StringHasher2.java
@@ -0,0 +1,29 @@
+package writeupExperiment;
+import providedCode.Hasher;
+
+/**
+ * 
+ * @author Reggie Jones
+ * CSE 332
+ * TA: Hye In Kim
+ * 
+ * StringHasher2 class is used for hashing Strings, while implementing Hasher<String>
+ * --For experimental purposes. hash function is intentionally very basic--
+ *
+ */
+public class StringHasher2 implements Hasher<String> {
+	
+	/**
+	 * hash returns a positive integer of the hash of a given String
+	 * @param s is a String to be hashed
+	 * @return an int that represents the hash of the String
+	 */
+	@Override
+	public int hash(String s) {
+		int hash = 0;
+		for (int i = 0; i < s.length(); i++) {
+			hash += (int) (s.charAt(i) * Math.pow(37, i));
+		}
+		return Math.abs(hash);
+	}
+}
